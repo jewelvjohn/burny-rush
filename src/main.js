e598fd4f34d112413158883017e1065bf7d5b8b6
Jewel John <jeweljohnl0v3@gmail.com>
2024-09-22 10:13:00 +0530

    initial commit!

---
+++ src/main.js
@@ -0,0 +1,12 @@
+import './style.css';
+import "@lottiefiles/lottie-player";
+import {create} from '@lottiefiles/lottie-interactivity';
+
+const instagram = document.getElementById("instagram");
+instagram.onload = (e) => {
+    create({
+        player:'#instagram',
+        mode:"cursor",
+        actions: [{type: "hold"}]
+    });
+}
