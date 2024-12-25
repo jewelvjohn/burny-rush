b8c859575fe4da9c76ec8ede0b231c7081185b4d
Jewel John <jeweljohnl0v3@gmail.com>
2024-12-25 20:37:05 +0530

    added: gameplay reel

--- src/main.js
+++ src/main.js
@@ -1,12 +1,55 @@
-import './style.css';
 import "@lottiefiles/lottie-player";
 import {create} from '@lottiefiles/lottie-interactivity';
 
-const instagram = document.getElementById("instagram");
-instagram.onload = (e) => {
-    create({
-        player:'#instagram',
-        mode:"cursor",
-        actions: [{type: "hold"}]
-    });
+function initializeLoading() {
+    const preloader = document.getElementById("preloader");
+    window.addEventListener("load", () => {
+        const animation = preloader.animate([
+            { opacity: 1 },
+            { opacity: 0 }
+        ], {
+            duration: 250,
+            fill: "forwards",
+            easing: "ease-out"
+        });
+        animation.addEventListener("finish", () => {
+            preloader.style.display = "none";
+        }, { once: true });
+    }, { once: true });
 }
+
+function initializeLottie() {
+    const instagram = document.getElementById("instagram");
+    instagram.onload = (e) => {
+        create({
+            player:'#instagram',
+            mode:"cursor",
+            actions: [{type: "hold"}]
+        });
+    }
+}
+
+function initilizeVideoPlayer() {
+    const video = document.getElementById("video");
+    const videoContainer = document.getElementById("video-container");
+    const playButton = document.getElementById("play-button");
+    videoContainer.onclick = () => {
+        if(video.paused) {
+            video.play();
+            playButton.classList.remove("paused");
+            playButton.classList.add("playing");
+        } else {
+            video.pause();
+            playButton.classList.remove("playing");
+            playButton.classList.add("paused");
+        }
+    };
+}
+
+function init() {
+    initializeLoading();
+    initializeLottie();
+    initilizeVideoPlayer();
+}
+
+init();
